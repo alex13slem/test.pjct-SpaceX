acaa1a54a5a7b165d8297c3ed9df1809f9f7c1df
alex13slem <a.s.scherba@yandex.by>
2022-08-01 14:17:11 +0300

    modal update 01.08.22

--- src/js/template.js
+++ src/js/template.js
@@ -2,14 +2,3 @@ import * as prjFunctions from "./modules/function.js";
 import 'bootstrap';
 
 prjFunctions.isWebp();
-
-
-// -------------------------------------------------------
-
-const header = document.querySelector('.header');
-const burgerBtn = header.querySelector('.burger');
-
-burgerBtn.addEventListener('click', evt => {
-
-	header.classList.toggle('header_open');
-})
